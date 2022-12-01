9801e6614551a0a5526382b9d365a5ff9dbae6b3
SuperFluffyGame <superfluffygamerminecraft@gmail.com>
2022-12-01 20:30:23 +0000

    2022-Day1

---
+++ 2022/day1/src/main.rs
@@ -0,0 +1,13 @@
+const INPUT: &str = include_str!("../input.txt");
+
+fn main() {
+    let mut elves: Vec<u32> = INPUT
+        .split("\n\n")
+        .map(|v| v.split('\n').map(|v| v.parse::<u32>().unwrap()).sum())
+        .collect();
+    elves.sort();
+    let top = elves[elves.len() - 1];
+    let top_three: u32 = elves.iter().rev().take(3).sum();
+    println!("Part 1: {:?}", top);
+    println!("Part 2: {:?}", top_three);
+}
