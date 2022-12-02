c19c42e456435c6217d31d78ffc0a0d699cbe6ba
SuperFluffyGame <81885757+SuperFluffyGame@users.noreply.github.com>
2022-12-01 21:43:29 -0600

    js day1

---
+++ 2022/js/day1/index.js
@@ -0,0 +1,20 @@
+// @ts-check
+import fs from "fs";
+const input = fs.readFileSync("./input.txt").toString();
+
+const elves = input.split("\r\n\r\n").map(v =>
+    v
+        .split("\r\n")
+        .map(v => parseInt(v))
+        .reduce((a, b) => a + b)
+);
+
+const top = elves.reduce((a, b) => (a > b ? a : b));
+
+const topThree = elves
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a + b);
+
+console.log(`Part 1: ${top}`);
+console.log(`Part 2: ${topThree}`);
