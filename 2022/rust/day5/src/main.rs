555adf91f893932dcd1566ac34d5fd477522e190
SuperFluffyGame <81885757+SuperFluffyGame@users.noreply.github.com>
2022-12-05 20:30:32 -0600

    Day 5 - Rust (shoulda used nom)

--- 2022/rust/day5/src/main.rs
+++ 2022/rust/day5/src/main.rs
@@ -24,7 +24,6 @@ fn part1(input: &str) -> String {
     let mut stacks = vec![vec![]; num_stacks];
 
     let stack_lines = input.lines().take(row_idx).collect::<Vec<_>>();
-    dbg!(&stack_lines);
     stack_lines.iter().enumerate().for_each(|(i, line)| {
         for stack_i in 0..num_stacks {
             let str_index = stack_i * 4;
@@ -43,36 +42,33 @@ fn part1(input: &str) -> String {
         .lines()
         .skip(row_idx + 2)
         .enumerate()
-        .map(|(i, line)| {
-            dbg!(line);
-
-            let o = (
-                line.chars()
-                    .skip(5)
-                    .take_while(|c| c.is_numeric())
-                    .collect::<String>()
-                    .parse::<usize>()
-                    .unwrap(),
-                {
-                    let str = line
-                        .chars()
-                        .skip(12)
-                        .take_while(|c| c.is_numeric())
-                        .collect::<String>();
-                    dbg!(&str);
-                    str
-                }
+        .map(|(_i, line)| {
+            let first = line
+                .chars()
+                .skip(5)
+                .take_while(|c| c.is_numeric())
+                .collect::<String>()
                 .parse::<usize>()
-                .unwrap_or_else(|e| panic!("{}", i)),
-                line.chars()
-                    .skip(17)
-                    .take_while(|c| c.is_numeric())
-                    .collect::<String>()
-                    .parse::<usize>()
-                    .unwrap(),
-            );
-            dbg!(&o);
-            o
+                .unwrap();
+
+            let second = line
+                .chars()
+                .skip(10)
+                .skip_while(|c| !c.is_numeric())
+                .take_while(|c| c.is_numeric())
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap();
+
+            let third = line
+                .chars()
+                .skip(14)
+                .skip_while(|c| !c.is_numeric())
+                .take_while(|c| c.is_numeric())
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap();
+            (first, second, third)
         })
         .for_each(|mov| {
             let (num, from, to) = mov;
@@ -86,8 +82,79 @@ fn part1(input: &str) -> String {
     stacks.iter().map(|stack| stack.last().unwrap()).collect()
 }
 
-fn part2(input: &str) -> u32 {
-    todo!()
+fn part2(input: &str) -> String {
+    // parsing the first part (by hand)
+    let (row_idx, num_row) = input
+        .lines()
+        .enumerate()
+        .find(|(_idx, line)| line.starts_with(" 1"))
+        .unwrap();
+    let num_stacks = num_row[(num_row.len() - 2)..(num_row.len() - 1)]
+        .parse::<usize>()
+        .unwrap();
+
+    let mut stacks = vec![vec![]; num_stacks];
+
+    let stack_lines = input.lines().take(row_idx).collect::<Vec<_>>();
+    stack_lines.iter().enumerate().for_each(|(i, line)| {
+        for stack_i in 0..num_stacks {
+            let str_index = stack_i * 4;
+
+            let char = line.chars().nth(str_index + 1).unwrap();
+            if !char.is_whitespace() {
+                stacks[stack_i].push(char);
+            }
+        }
+    });
+    stacks.iter_mut().for_each(|stack| {
+        stack.reverse();
+    });
+
+    input
+        .lines()
+        .skip(row_idx + 2)
+        .enumerate()
+        .map(|(_i, line)| {
+            let first = line
+                .chars()
+                .skip(5)
+                .take_while(|c| c.is_numeric())
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap();
+
+            let second = line
+                .chars()
+                .skip(10)
+                .skip_while(|c| !c.is_numeric())
+                .take_while(|c| c.is_numeric())
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap();
+
+            let third = line
+                .chars()
+                .skip(14)
+                .skip_while(|c| !c.is_numeric())
+                .take_while(|c| c.is_numeric())
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap();
+            (first, second, third)
+        })
+        .for_each(|mov| {
+            let (num, from, to) = mov;
+
+            let mut pops = Vec::new();
+            for _ in 0..num {
+                let v = stacks[from - 1].pop().unwrap();
+                pops.push(v);
+            }
+            pops.reverse();
+            stacks[to - 1].append(&mut pops);
+        });
+
+    stacks.iter().map(|stack| stack.last().unwrap()).collect()
 }
 
 fn main() {
